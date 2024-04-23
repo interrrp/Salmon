1f5edb138cd4ce4002a4a7f3eb07ba112480f56b
interrrp <int@addictedto.coffee>
2024-04-23 16:27:24 +0800

    Create TUI chess interface

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Chess;
+
+var board = new ChessBoard();
+var errored = false;
+
+while (!board.IsEndGame)
+{
+    Console.Clear();
+
+    Console.ForegroundColor = ConsoleColor.DarkGray;
+    Console.WriteLine(board.ToPgn() + "\n");
+
+    Console.ForegroundColor = ConsoleColor.Gray;
+    Console.WriteLine(board.ToAscii());
+
+    Console.ForegroundColor = errored ? ConsoleColor.Red : ConsoleColor.Blue;
+    Console.Write("san → ");
+    Console.ForegroundColor = ConsoleColor.White;
+    try
+    {
+        board.Move(Console.ReadLine()!);
+        errored = false;
+    }
+    catch (Exception)
+    {
+        errored = true;
+    }
+}
+
