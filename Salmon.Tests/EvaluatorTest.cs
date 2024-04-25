598900408dd0eb335b796a303bbcdb377fe3074c
interrrp <int@addictedto.coffee>
2024-04-25 17:18:30 +0800

    Restructure and add tests

---
+++ Salmon.Tests/EvaluatorTest.cs
@@ -0,0 +1,22 @@
+using Chess;
+
+namespace Salmon.Tests;
+
+public class EvaluatorTest
+{
+    [Fact]
+    public void Evaluate_WhiteWins_ReturnsBestScore()
+    {
+        // Bongcloud or whatever
+        var board = ChessBoard.LoadFromFen("rnbq1bnr/ppppkppp/8/4Q3/4P3/8/PPPP1PPP/RNB1KBNR b KQ - 0 3");
+        Assert.Equal(board.Evaluate(), PieceColor.White.BestScore());
+    }
+
+    [Fact]
+    public void Evaluate_BlackWins_ReturnsBestScore()
+    {
+        // Classic Fool's Mate
+        var board = ChessBoard.LoadFromFen("rnb1kbnr/pppp1ppp/8/4p3/6Pq/5P2/PPPPP2P/RNBQKBNR w KQkq - 1 3");
+        Assert.Equal(board.Evaluate(), PieceColor.Black.BestScore());
+    }
+}
